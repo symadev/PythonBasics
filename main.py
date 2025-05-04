c63b3469b7d72a3f6b184748b6d2e96533185666
syma sultana <symasultana2002@gmail.com>
2025-05-04 13:42:51 +0600

    added some arithmetic operations

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ print(5)
 print("Hi, my name is rasha")
 print("Hi, my name is rasha, i am going to learn python now")
 
+a= 2
+b=3
+sum = a+b
+print (sum)
+
+# arithmeic operators
+a= 2
+b=3
+
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
