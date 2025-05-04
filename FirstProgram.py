c9760460cb8b602bb4f34ab18df44655be445f41
syma sultana <symasultana2002@gmail.com>
2025-05-04 13:18:02 +0600

    first commit

---
+++ FirstProgram.py
@@ -0,0 +1,12 @@
+print("Hi, rasha")
+print(5)
+print(125+134)
+print("Hi, my name is rasha")
+print("Hi, my name is rasha, i am going to learn python now")
+name = "Rasha"
+age = 23
+profession = "Web Developer"
+print(name,age,profession )
+print(type(name))
+print(type(age))
+print(type(profession))
---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hi, rasha")
+print(5)
+print("Hi, my name is rasha")
+print("Hi, my name is rasha, i am going to learn python now")
+
