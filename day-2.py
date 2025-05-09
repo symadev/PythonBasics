d4641ae0811d648e781722fa1fb6e497209f341d
syma sultana <symasultana2002@gmail.com>
2025-05-10 00:15:15 +0600

    add concatination and length

---
+++ day-2.py
@@ -0,0 +1,15 @@
+str1= "this is my homepage"
+len1= len(str1)
+str2= "you are mine.\tand i am with you"
+str3= "this is my python code.\nwe are creating it in python"
+finalStr= str2+ " "+ str3
+print (str3)
+print (str2)
+print (finalStr)
+
+#concatination
+str4= "hello"
+str5= "world"
+print(str4+str5)
+#length
+print(len1)
