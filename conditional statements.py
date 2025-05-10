4286f33c140c111bbd9895c13de9363ddb4f567e
syma sultana <symasultana2002@gmail.com>
2025-05-10 19:00:07 +0600

    added more examples

---
+++ conditional statements.py
@@ -0,0 +1,36 @@
+# age = 23
+# if (age >= 18):
+#     print("go ahead")
+#     print("keep it up")
+#     print("make it togather")
+
+    # more example
+light ="pink"
+if(light == "red"):
+ print("just stop")
+elif(light == "yellow"):
+ print(" just look")
+elif(light == "green"):
+ print("just go")
+else:
+ print ("light broken")
+
+ print("end of code")
+
+
+ #
+ # example
+
+marks = 90
+if(marks >= 90):
+    grade = "A"
+elif(marks >= 70 and marks <= 80):
+  grade="B"
+elif(marks >= 50 and marks <= 60):
+  grade= "C"
+else:
+  grade= "D"
+
+print ("all the students marks is -->", grade)
+
+
