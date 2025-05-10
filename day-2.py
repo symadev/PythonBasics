b18d2d6dc3a9d61f0d08e55b5ccb06b23f12faf1
syma sultana <symasultana2002@gmail.com>
2025-05-10 17:25:25 +0600

    added the strings functions

--- day-2.py
+++ day-2.py
@@ -5,11 +5,59 @@ str3= "this is my python code.\nwe are creating it in python"
 finalStr= str2+ " "+ str3
 print (str3)
 print (str2)
-print (finalStr)
+# print (finalStr)
 
 #concatination
 str4= "hello"
 str5= "world"
 print(str4+str5)
+
+
+
 #length
 print(len1)
+str6 = "my college"
+ch = str6[0]
+print (ch)
+ch1 = str6[4]
+print (ch1)
+
+
+
+# this process is called slicing
+print (str6[0:2])
+print (str6[3:10])
+print (str6[4:8])
+print (str6[3:len(str6)])
+
+
+# using negetive index
+strMain = "apple"
+print (strMain[-3:-1])
+print (strMain[-5:-1])
+print (strMain[-5:])
+
+#strings functions example
+
+
+# using endswith
+print (strMain.endswith("ple"))
+print (strMain.endswith("app"))
+# aikehne bojhacce j app te shesh hocce kina string ta, thik hole true ashbe ar na hoile false ashbe
+
+
+# using capitalize
+print(strMain.capitalize())
+
+# using replace
+print(strMain.replace("p", "r"))
+print(strMain.replace("apple", "banana"))
+
+
+# using find
+print(strMain.find("e"))
+# result a ar index value ta show korbe
+print(strMain.find("h"))
+
+# using count
+print(strMain.count("p"))
