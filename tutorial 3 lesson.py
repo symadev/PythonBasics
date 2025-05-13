13e030b87f581357dc38045fdf11946198409e8a
syma sultana <symasultana2002@gmail.com>
2025-05-13 15:14:24 +0600

    added the list method

---
+++ tutorial 3 lesson.py
@@ -0,0 +1,39 @@
+# list example
+# marks = 23
+# marks= 56
+# marks = 67
+# marks =235
+marks = [45,67,98,43,789,8653]
+print(marks)
+
+print(type(marks))
+all=["monika", 67, 78.9, "rashi"]
+print(all)
+print(all[1:3])
+print(all[0:3])
+
+
+
+
+# list method
+
+num= [3,2,1]
+print(num.append(4))
+print(num)
+
+
+print(list.sort(num))
+print(num)
+
+
+# sort to decending order
+print(num.sort(reverse=True))
+print(num)
+
+
+# sort to acending order
+print(num.reverse())
+print(num)
+
+
+
