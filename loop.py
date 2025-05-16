d542a5141fc996b84bc2d3e6f84e6efa0e6076b8
syma sultana <symasultana2002@gmail.com>
2025-05-16 20:10:07 +0600

    added the loop examle

---
+++ loop.py
@@ -0,0 +1,8 @@
+count = 1
+while count<=5 :
+    print("hello")
+    count +=1
+    print(count)
+    # 5 count true  na howa ponto aikahne condition cheak korte thakbe and true hoye gele lop stop hoye jabe
+    # count is called iterator
+    # in this whole procedure is called iteration
