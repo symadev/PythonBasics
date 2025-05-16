34131ed154d8ad4b6a6dfe372b9b6ffe5b2cf43a
syma sultana <symasultana2002@gmail.com>
2025-05-16 10:22:48 +0600

    added the nested dict

--- dictionary.py
+++ dictionary.py
@@ -3,8 +3,39 @@ info = {
     "name": "ritu",
     "college": "shipyard",
     12:34.90,
+    "topics":( "dict", "set"),
     "is_adult": True,
     "marks" : [23, 44,3,1,4],
 
 }
+print(type(info))
+info["sername"] = ["masha"]
 print(info)
+print(info["topics"])
+
+#nested dictionary
+
+infoMain = {
+    "key": "value",
+    "name": "ritu",
+    "college": "shipyard",
+    12:34.90,
+    "topics":( "dict", "set"),
+    "is_adult": True,
+
+    "student" : {
+    "marks" : [23, 44,3,1,4],
+    "subject":["python","c","java"],
+    "numbers": [3,55,34,24,],
+},
+    "mainSub":"bangla",
+
+}
+print(infoMain)
+print(infoMain.keys())
+print(infoMain.values())
+#for covert in the type casting
+print(list(infoMain.values()))
+print(list(infoMain.keys()))
+
+
